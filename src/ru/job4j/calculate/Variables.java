9092c1aa485836fabe26b6e624373687ca778ab6
Constantine-M <mez365@mail.ru>
2020-09-02 18:20:06 +0300

    2. Переменные. [#347445]

--- src/ru/job4j/calculate/Variables.java
+++ src/ru/job4j/calculate/Variables.java
@@ -2,11 +2,28 @@ package ru.job4j.calculate;
 
 public class  Variables {
     public static void main(String[] args) {
-        int age = 18;
+        int age = 128;
         System.out.println(age);
         float length = 78.0332F;
         System.out.println(length);
         boolean baby = true;
         System.out.println(baby);
+        char gender = 'M';
+        System.out.println(gender);
+        float height = 2.51F;
+        System.out.println(height);
+        int brick = 56015;
+        System.out.println(brick);
+        long bacteria = 1564784777;
+        System.out.println(bacteria);
+        double distance = 15467.189785148;
+        System.out.println(distance);
+        boolean work = true;
+        System.out.println(work);
+        double size = 14895.1844951959;
+        System.out.println(size);
+
+
+
     }
 }
