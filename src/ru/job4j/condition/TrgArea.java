2c0f1f51b02a217cfa1ad973d78f0287910787bb
Constantine-M <mez365@mail.ru>
2020-09-12 22:50:59 +0300

    9.1. String [#347450]

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -4,15 +4,15 @@ import java.lang.Math;
 import java.text.DecimalFormat;
 
 public class TrgArea {
-    public static double area(double a,double b,double c) {
-        double p = (a + b + c)/2;
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
         double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return  rsl;
     }
 
-    public static void  main(String[] args) {
+    public static void main(String[] args) {
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
-        double rsl = TrgArea.area(2,2,2);
+        double rsl = TrgArea.area(2, 2, 2);
         String rslmod = decimalFormat.format(rsl);
         System.out.println("area (2, 2, 2) = " + rslmod);
     }
