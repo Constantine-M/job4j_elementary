8eb2759df6fcefb5d003bb49ab1f31e5b40ea46e
Constantine-M <mez365@mail.ru>
2020-09-12 22:05:00 +0300

    String

---
+++ src/ru/job4j/calculate/Out.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String lyrics = "Мама мыла раму";
+        System.out.println(lyrics);
+        String name = "Constantine";
+        String surname = "Mezenin";
+        String separate = " - ";
+        byte age = 25;
+        String id = name + separate + age + " years old";
+        System.out.println(id);
+        int first = 1;
+        int second = 2;
+        int onePlustwo = first + second;
+        System.out.println("1 + 2 = " + onePlustwo);
+    }
+}
