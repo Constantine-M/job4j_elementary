9e9c0e1462e1240183c5d0f6e573601ae3a36a9e
Constantine-M <mez365@mail.ru>
2020-09-12 21:52:41 +0300

    Упражнения

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Constantine Mezenin");
+        System.out.println("06.03.1995");
+    }
+}
