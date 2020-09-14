60af0d2810ab3818f5efaf844701ce4bac4ebe75
Constantine-M <mez365@mail.ru>
2020-09-14 11:58:02 +0300

    9.1. String [#347450] правки

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -3,11 +3,11 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
-        String student = idea + " But I am a newbie.";
-        System.out.println(student);
+        System.out.println(idea);
+        idea = idea + " But I am a newbie. ";
         int year = 2020;
-        String idyear = idea + year;
-        System.out.println(idyear);
+        idea = idea + year;
+        System.out.println(idea);
 
     }
 }
