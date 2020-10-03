878f98d894697b591d757d6b1a37949dd0aa91eb
Constantine-M <mez365@mail.ru>
2020-10-03 13:32:18 +0300

    4. Тесты для стороны прямоугольника

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 import java.text.DecimalFormat;
 
 public class SqArea {
-    public static double square(int p, int k) {
+    public static double square(double p, double k) {
         double height = p / (2 * (k + 1));
         double length = height * k;
         double rsl = length * height;
@@ -12,8 +12,8 @@ public class SqArea {
 
     public static void main(String[] args) {
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
-        double result = SqArea.square(3, 4);
+        double result = SqArea.square(4, 1);
         String result1 = decimalFormat.format(result);
-        System.out.println(" p = 4, k = 1, s = 2, real = " + result);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
     }
 }
