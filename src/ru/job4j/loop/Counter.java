2f40b587afeae31150435d87ebc7a1755c2b1c44
Constantine-M <mez365@mail.ru>
2020-10-24 12:53:37 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#347503]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -10,10 +10,14 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 5));
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        int i = start;
+        for (i = start; i <= finish; i++) {
+            if (i % 2 == 0 && finish % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
     }
 }
---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void sumByEvenFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        assertThat(rsl, is(30));
+    }
+
+    @Test
+    public void sumByEvenFromZeroToTenThenThirty() {
+        int rsl = Counter.sumByEven(0, 10);
+        assertThat(rsl, is(30));
+    }
+
+    @Test
+    public void sumByEvenFromThreeToEightThenEighteen() {
+        int rsl = Counter.sumByEven(3, 8);
+        assertThat(rsl, is(18));
+    }
+
+    @Test
+    public void sumByEvenFromOneToOneThenOne() {
+        int rsl = Counter.sumByEven(1, 1);
+        assertThat(rsl, is(0));
+    }
+}
