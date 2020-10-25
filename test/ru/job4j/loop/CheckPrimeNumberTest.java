e26f4addc44ba12b1d9fc60e839b2f60db2d32e3
Constantine-M <mez365@mail.ru>
2020-10-25 14:05:38 +0300

    5.5. Простое число [#347498]

---
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void checkWhenFive() {
+        boolean rsl = CheckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void checkWhenFour() {
+        boolean rsl = CheckPrimeNumber.check(4);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void checkWhenOne() {
+        boolean rsl = CheckPrimeNumber.check(1);
+        assertThat(rsl, is(false));
+    }
+}
