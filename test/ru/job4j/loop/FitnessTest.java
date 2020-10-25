fbc92ce74fdef9b07cc5d9ca58dabff06643fb4a
Constantine-M <mez365@mail.ru>
2020-10-25 11:45:07 +0300

    5.4. Протеиновая диета [#347495]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        int rsl = Fitness.calc(95, 90);
+        assertThat(rsl, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        int rsl = Fitness.calc(90, 95);
+        assertThat(rsl, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        int rsl = Fitness.calc(50, 90);
+        assertThat(rsl, is(2));
+    }
+}
