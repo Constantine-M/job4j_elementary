673d6fc929e5fd6ee68d57eb77a713bfeb77d891
Constantine-M <mez365@mail.ru>
2020-12-06 20:52:53 +0300

    9. Размер двумерного массива

---
+++ src/ru/job4j/array/Board.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class Board {
+    public static int getCountRow(int[][] array) {
+        return array.length;
+    }
+    //посмотреть кол-во элементов в ряду
+    public static int getCountInRow(int[][] array, int row) {
+        return array[row].length;
+    }
+}
